a161fb45524b229df121d6ea2df9f4c392a89f64
Shariq Ahmed <shariq.ahmed525@gmail.com>
2026-08-14 21:54:01 +0500

    feat: implement dynamic asset path resolution and update CI/CD environment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
     remotePatterns: [{ protocol: "https", hostname: "images.prismic.io" }],
